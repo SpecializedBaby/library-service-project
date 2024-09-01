db63cd06ad1de2f7320b0b660bb0b6e17bf0a420
Dede <specialized8393@gmail.com>
2024-09-01 17:25:54 +0100

    Add Book model and run initial migrations
    - Created Book model with fields: title, author, published_date, isbn_number, number_of_pages, cover_image, and language
    - Generated and applied initial database migrations for the Book model

--- book/models.py
+++ book/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField()
+    isbn_number = models.CharField(max_length=13, unique=True)
+    number_of_pages = models.PositiveIntegerField()
+    cover_image = models.ImageField(upload_to="cover_image/", null=True, blank=True)
+    language = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
