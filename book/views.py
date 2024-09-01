88cabff7e80362c6286928d6e744a9026356f3a6
Dede <specialized8393@gmail.com>
2024-09-01 20:27:52 +0100

    Add custom permissions to BookViewSet: admin-only for create/update/delete, open for list
    - Updated BookViewSet to restrict create, update, and delete actions to admin users only
    - Allowed all users, including unauthenticated, to list books via API
    - Configured get_permissions method in BookViewSet to enforce custom permissions per action

--- book/views.py
+++ book/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAdminUser
 
 from .models import Book
 from .serializers import BookSerializer
@@ -9,3 +10,11 @@ from .serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    def get_permissions(self):
+        if self.action in ["list"]:
+            self.permission_classes = [AllowAny, ]
+        else:
+            self.permission_classes = [IsAdminUser, ]
+
+        return super().get_permissions()
