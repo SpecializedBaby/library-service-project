3eecc58a1cc7d2686909ca494dbdc9a13603919a
Dede <specialized8393@gmail.com>
2024-09-02 19:56:50 +0100

    Created serializers and viewsets for Borrowing API
    - Updated project URLs to include borrowings endpoints

--- library_service/urls.py
+++ library_service/urls.py
@@ -23,5 +23,6 @@ from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/book/", include("book.urls", namespace="book")),
-    path("api/user/", include("user.urls", namespace="user"))
+    path("api/user/", include("user.urls", namespace="user")),
+    path("api/borrowing/", include("borrowing.urls", namespace="borrowing"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
