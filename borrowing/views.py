8cd30eeb8c8f3d3b8668c070066c3bc13d830f6c
Dede <specialized8393@gmail.com>
2024-09-03 20:30:18 +0100

    Add filtering for the Borrowings List endpoint
    - Only user can see own borrowings
    - admin can see all borrowings
    - Implemented is_active filter to show only active borrowings
    - Added user_id filter for admin users to filter borrowings by specific user

--- borrowing/views.py
+++ borrowing/views.py
@@ -17,6 +17,27 @@ class BorrowingViewSet(
         permissions.IsAuthenticated,
     ]
 
+    @staticmethod
+    def _params_to_ints(qs):
+        """Converts a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in qs.split(",")]
+
+    def get_queryset(self):
+        is_active = self.request.query_params.get("is_active")
+        user_id = self.request.query_params.get("user_id")
+        user = self.request.user
+        queryset = self.queryset
+
+        if user_id:
+            user_id = self._params_to_ints(user_id)
+            queryset = queryset.filter(user__id__in=user_id)
+
+        if is_active:
+            queryset = queryset.filter(actual_return_date__isnull=True)
+        if not user.is_staff:
+            queryset = queryset.filter(user=self.request.user)
+        return queryset.distinct()
+
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
