47ce91f953bf28223c8e3f7d98eb4e6ef908a1cb
Dede <specialized8393@gmail.com>
2024-09-03 15:07:30 +0100

    Added id and updated book to the fields BorrowingSerializer

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -1,15 +1,20 @@
 from rest_framework import serializers
 
 from borrowing.models import Borrowing
+from book.serializers import BookSerializer
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
+    book = BookSerializer()
+
     class Meta:
         model = Borrowing
         fields = (
+            "id",
             "user",
             "book",
             "borrow_date",
             "expected_return_date",
             "actual_return_date",
         )
+        read_only_fields = ("id",)
