7f4fb5f9267949cec6bb9616e570323e4d9d9fac
Dede <specialized8393@gmail.com>
2024-09-03 15:08:54 +0100

    Added inventory, daily_fee, cover to Book model and serializer

--- book/models.py
+++ book/models.py
@@ -1,11 +1,18 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Book(models.Model):
+    class CoverChoices(models.TextChoices):
+        HARD = "HARD", _("Hardcover")
+        SOFT = "SOFT", _("Softcover")
+
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
+    cover = models.CharField(max_length=4, choices=CoverChoices.choices, default=CoverChoices.SOFT)
+    daily_fee = models.DecimalField(max_digits=6, decimal_places=2, help_text=_("Daily fee in USD"))
     published_date = models.DateField()
-    isbn_number = models.CharField(max_length=13, unique=True)
+    inventory = models.PositiveIntegerField()
     number_of_pages = models.PositiveIntegerField()
     cover_image = models.ImageField(upload_to="cover_image/", null=True, blank=True)
     language = models.CharField(max_length=50)
