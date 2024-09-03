6cd1a85c38f7b12a2963404c0f94c4224e7e2e40
Dede <specialized8393@gmail.com>
2024-09-03 16:48:01 +0100

    Implement Create Borrowing endpoint
    - Updated BorrowingSerializer to handle borrowing validation and creation logic
    - Ensured book inventory is not zero before creating a borrowing
    - Decreased book inventory by 1 upon successful borrowing creation
    - Attached the current user to the borrowing instance
    - Updated BorrowingViewSet to handle the borrowing creat, list and detail endpoint

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -1,3 +1,5 @@
+from django.utils import timezone
+
 from rest_framework import serializers
 
 from borrowing.models import Borrowing
@@ -17,4 +19,32 @@ class BorrowingSerializer(serializers.ModelSerializer):
             "expected_return_date",
             "actual_return_date",
         )
-        read_only_fields = ("id",)
+        read_only_fields = ("id", "borrow_date", "actual_return_date",)
+
+    def validate(self, attrs):
+        """
+        Ensure that the book inventory is greater than 0 and the expected return date is after the borrow date.
+        """
+
+        book = attrs.get("book")
+
+        if book.inventory <= 0:
+            raise serializers.ValidationError("The selected book is not available in the inventory.")
+
+        if attrs.get("expected_return_date") <= timezone.now():
+            raise serializers.ValidationError("The expected return date must be in the future.")
+
+        return attrs
+
+    def create(self, validated_data):
+        """
+        Custom create method to decrease inventory and attach the current user.
+        """
+
+        book = validated_data["book"]
+        book.inventory -= 1
+        book.save()
+
+        borrowing = Borrowing.objects.create(user=self.context["request"].user, **validated_data)
+
+        return borrowing
--- borrowing/views.py
+++ borrowing/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import generics, permissions
+from rest_framework.viewsets import GenericViewSet
+from rest_framework import viewsets, mixins
 
 from borrowing.models import Borrowing
 from borrowing.serializers import BorrowingSerializer
 
 
-class BorrowingViewSet(viewsets.ModelViewSet):
+class BorrowingViewSet(mixins.CreateModelMixin,
+                       mixins.ListModelMixin,
+                       mixins.RetrieveModelMixin,
+                       GenericViewSet):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+    permission_classes = [permissions.IsAuthenticated, ]
 
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
